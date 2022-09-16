319ca6925f6bbeb397fec689702b1e8f4d1c03f8
msobocinska <msobocinska@sii.pl>
2022-09-16 16:07:17 +0200

    faker, getRandomUser()

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,104 @@
 package models;
 
+import java.util.Date;
+
 public class User {
+
+    private SocialTitle socialTitle;
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String password;
+    private String birthdate;
+    private boolean receiveOffersConsent;
+    private boolean customerDataPrivacyConsent;
+    private boolean newsletterConsent;
+    private boolean generalConditionsConsent;
+
+
+    public User(SocialTitle socialTitle, String firstName, String lastName, String email, String password, String birthdate, boolean receiveOffersConsent, boolean customerDataPrivacyConsent, boolean newsletterConsent, boolean generalConditionsConsent) {
+        this.socialTitle = socialTitle;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.birthdate = birthdate;
+        this.receiveOffersConsent = receiveOffersConsent;
+        this.customerDataPrivacyConsent = customerDataPrivacyConsent;
+        this.newsletterConsent = newsletterConsent;
+        this.generalConditionsConsent = generalConditionsConsent;
+    }
+
+    enum SocialTitle {
+        MR, MRS
+    }
+
+    public static class UserBuilder {
+        private SocialTitle socialTitle;
+        private String firstName;
+        private String lastName;
+
+        private String email;
+        private String password;
+        private Date birthdate;
+        private boolean receiveOffersConsent;
+        private boolean customerDataPrivacyConsent;
+        private boolean newsletterConsent;
+        private boolean generalConditionsConsent;
+
+        public UserBuilder setSocialTitle(SocialTitle socialTitle) {
+            this.socialTitle = socialTitle;
+            return this;
+        }
+
+        public UserBuilder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setBirthdate(Date birthdate) {
+            this.birthdate = birthdate;
+            return this;
+        }
+
+        public UserBuilder setReceiveOffersConsent(boolean receiveOffersConsent) {
+            this.receiveOffersConsent = receiveOffersConsent;
+            return this;
+        }
+
+        public UserBuilder setCustomerDataPrivacyConsent(boolean customerDataPrivacyConsent) {
+            this.customerDataPrivacyConsent = customerDataPrivacyConsent;
+            return this;
+        }
+
+        public UserBuilder setNewsletterConsent(boolean newsletterConsent) {
+            this.newsletterConsent = newsletterConsent;
+            return this;
+        }
+
+        public UserBuilder setGeneralConditionsConsent(boolean generalConditionsConsent) {
+            this.generalConditionsConsent = generalConditionsConsent;
+            return this;
+        }
+
+        public User build() {
+            return new User(socialTitle, firstName, lastName, email, password, birthdate, receiveOffersConsent, customerDataPrivacyConsent, newsletterConsent, generalConditionsConsent);
+        }
+    }
 }
--- src/main/java/models/UserFactory.java
+++ src/main/java/models/UserFactory.java
@@ -1,4 +1,45 @@
 package models;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+
 public class UserFactory {
+
+    public User getRandomUser() {
+        Faker faker = new Faker();
+
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("pl-PL"), new RandomService());
+        String email = fakeValuesService.bothify("????##@gmail.com");
+
+        String password = faker.internet().password();
+        Date birthdate = faker.date().birthday();
+        boolean receiveOffersConsent = new Random().nextBoolean();
+        boolean customerDataPrivacyConsent = new Random().nextBoolean();
+        boolean newsletterConsent = new Random().nextBoolean();
+        boolean generalConditionsConsent = new Random().nextBoolean();
+
+        return new User.UserBuilder()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setPassword(password)
+                .setBirthdate(birthdate)
+                .setReceiveOffersConsent(receiveOffersConsent)
+                .setCustomerDataPrivacyConsent(customerDataPrivacyConsent)
+                .setNewsletterConsent(newsletterConsent)
+                .setGeneralConditionsConsent(generalConditionsConsent)
+                .build();
+    }
+
+    public User getAlreadyRegisteredUser() {
+
+    }
 }
