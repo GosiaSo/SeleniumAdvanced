e5e910b906c91704c2122bb505630cf8ead8c398
msobocinska <msobocinska@sii.pl>
2022-09-18 19:21:39 +0200

    search test 2

--- src/main/java/pages/common/HeaderPage.java
+++ src/main/java/pages/common/HeaderPage.java
@@ -1,13 +1,20 @@
 package pages.common;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pages.PageBase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HeaderPage extends PageBase {
 
+    private static final Logger logger = LoggerFactory.getLogger(HeaderPage.class);
     public HeaderPage(WebDriver driver) {
         super(driver);
     }
@@ -18,9 +25,27 @@ public class HeaderPage extends PageBase {
     @FindBy(css = "#search_widget button")
     private WebElement submitSearchButton;
 
+    @FindBy(css = ".ui-autocomplete li ")
+    private List<WebElement> autocompleteList;
+
 
     public void useSearchEngine(String searchText) {
         sendKeysWithClear(searchInput, searchText);
+    }
+
+    public void enterSearch() {
         actions.keyDown(Keys.ENTER).perform();
+    }
+
+    public List<String> retrieveAutocompleteSearchItems(String searchText) {
+        List<String> autocompleteSearchItems = new ArrayList<>();
+        useSearchEngine(searchText);
+        for (WebElement webElement : autocompleteList) {
+            waitToBeClickable(webElement);
+            String dropdownItem = webElement.findElement(By.cssSelector("span.product")).getText();
+            autocompleteSearchItems.add(dropdownItem);
+            logger.debug(dropdownItem);
+        }
+        return autocompleteSearchItems;
     }
 }
--- src/test/java/search/SearchTest.java
+++ src/test/java/search/SearchTest.java
@@ -3,12 +3,15 @@ package search;
 import BaseTest.BaseTest;
 import models.User;
 import models.UserFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pages.common.HeaderPage;
 import pages.homepage.ProductTilePage;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +20,7 @@ public class SearchTest extends BaseTest {
     private static final Logger logger = LoggerFactory.getLogger(SearchTest.class);
 
     @Test
-    // test for test user factory
+        // test for test user factory
     void xxxx() {
         String actualTitle = driver.getTitle();
         UserFactory userFactory = new UserFactory();
@@ -27,6 +30,8 @@ public class SearchTest extends BaseTest {
     }
 
     @Test
+    @Tag("search")
+    @Tag("regression")
     void checkResultOfSearch() {
 
         ProductTilePage productTilePage = new ProductTilePage(driver);
@@ -38,11 +43,33 @@ public class SearchTest extends BaseTest {
         String nameOfRandomProduct = productTilePage.getNameOfProduct(randomProductFromList);
 
         headerPage.useSearchEngine(nameOfRandomProduct);
+        headerPage.enterSearch();
         if (!productTilePage.checkIfSearchFundItems(initialNumberOfElements)) {
             assertThat(Boolean.TRUE).isEqualTo(Boolean.FALSE);
         }
         String result = productTilePage.getNameOfProduct(productTilePage.retrieveSpecificTile(nameOfRandomProduct));
-
         assertThat(result).isEqualTo(nameOfRandomProduct);
+    }
+
+    @Test
+    @Tag("search")
+    @Tag("regression")
+    void checkDropdownSearch() {
+        final String SEARCH_INPUT = "HUMMINGBIRD";
+        ProductTilePage productTilePage = new ProductTilePage(driver);
+        HeaderPage headerPage = new HeaderPage(driver);
+
+        headerPage.useSearchEngine(SEARCH_INPUT);
+        List<String> autocompleteSearchItems = headerPage.retrieveAutocompleteSearchItems(SEARCH_INPUT);
+
+        boolean flag = Boolean.TRUE;
+        for (String listItem : autocompleteSearchItems) {
+            if (listItem.toLowerCase().contains(SEARCH_INPUT.toLowerCase())) {
+                continue;
+            } else {
+                flag = Boolean.FALSE;
+            }
+        }
+        assertThat(flag).isEqualTo(Boolean.TRUE);
     }
 }
