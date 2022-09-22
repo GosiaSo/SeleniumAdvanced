1ac060c8f782d24c54e36cdc436913fce8f68d82
msobocinska <msobocinska@sii.pl>
2022-09-22 14:25:58 +0200

    subcategories

--- src/main/java/pages/common/HeaderPage.java
+++ src/main/java/pages/common/HeaderPage.java
@@ -1,6 +1,5 @@
 package pages.common;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -26,6 +25,23 @@ public class HeaderPage extends PageBase {
     @FindBy(css = ".top-menu")
     private WebElement menu;
 
+    @FindBy(css = "#category-3 a")
+    private WebElement clothesMainCategory;
+
+    @FindBy(css = "#category-6 a")
+    private WebElement accessoriesMainCategory;
+
+    @FindBy(css = "li#category-4 a")
+    private WebElement menSubcategory;
+
+    @FindBy(css = "li#category-5 a")
+    private WebElement womenSubcategory;
+
+    @FindBy(css = "li#category-7 a")
+    private WebElement stationerySubcategory;
+
+    @FindBy(css = "li#category-8 a")
+    private WebElement homeAccessoriesSubcategory;
 
     public List<WebElement> getMainCategoriesMenuItems() {
         waitToBeVisible(menu);
@@ -36,26 +52,56 @@ public class HeaderPage extends PageBase {
         return additionalCategoryMenuItems;
     }
 
+    public WebElement getClothesMainCategory() {
+        return clothesMainCategory;
+    }
+
+    public WebElement getAccessoriesMainCategory() {
+        return accessoriesMainCategory;
+    }
+
+    public WebElement getMenSubcategory() {
+        return menSubcategory;
+    }
+
+    public WebElement getWomenSubcategory() {
+        return womenSubcategory;
+    }
+
+    public WebElement getStationerySubcategory() {
+        return stationerySubcategory;
+    }
+
+    public WebElement getHomeAccessoriesSubcategory() {
+        return homeAccessoriesSubcategory;
+    }
+
     public void selectCategory(WebElement element) {
         click(element);
     }
 
     public void selectCategory(String category) {
         for (int i = 0; i < mainCategoriesMenu.size(); i++) {
-            if(mainCategoriesMenu.get(i).getText().equalsIgnoreCase(category)){
+            if (mainCategoriesMenu.get(i).getText().equalsIgnoreCase(category)) {
                 click(mainCategoriesMenu.get(i));
             }
         }
     }
 
     public String getNameOfMenuElement(WebElement element) {
-        waitToBeVisible(element);
         return element.getText();
     }
 
-    public void selectAdditionalCategory(WebElement element) {
+    public String getNameOfSubCategoryElement(WebElement element) {
+        return element.getAttribute("innerHTML").trim();
+    }
+
+    public void clickAdditionalCategory(WebElement mainCategory, WebElement additionalCategory) {
         waitToBeVisible(menu);
-        actions.moveToElement(element).perform();
-        List<WebElement> additionalCategories = element.findElements(By.className("category"));
+        waitToBeVisible(mainCategory);
+
+        actions.moveToElement(mainCategory).build().perform();
+        waitToBeVisible(additionalCategory);
+        click(additionalCategory);
     }
 }
