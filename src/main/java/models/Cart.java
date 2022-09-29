feb86db50d48a6be3a212615a089deec790f3e53
msobocinska <msobocinska@sii.pl>
2022-09-29 14:24:29 +0200

    Product Model, ProductFactory Builder, added random products to basket

---
+++ src/main/java/models/Cart.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.util.List;
+
+public class Cart {
+
+    private List<Product> products;
+    private double totalCost;
+
+    public Cart(List<Product> products, double totalCost) {
+        this.products = products;
+        this.totalCost = totalCost;
+    }
+
+    public void addToList(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+}
